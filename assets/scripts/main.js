de7d4c6f7753d175e35e567b23a5bdfef1184ca3
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2022-11-27 20:18:27 +0200

    store in the browser and make popup conformation messege

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,31 @@
 const task=document.querySelector('.task');
-let tasks=[
-];
 const add=document.querySelector('.add')
-const todos=document.querySelector('.todos')
-
+const toDos=document.querySelector('.todos')
 const counter=document.querySelector('.count-tasks')
 let search=document.getElementById("search");
 let count =0;
+let tasks=[];
+let remove=false;
 displayTasks();
+if(localStorage.getItem("alldata")!=null){
+    tasks = JSON.parse(localStorage.getItem("alldata"));
+    count=tasks.length;
+    setTaskCounter()
+    
+    displayTasks();
+}else {
+tasks =[];
 
-
+}
+/*********************/
 add.addEventListener('click',handleAddition);
 search.addEventListener('keyup',searchTasks)
-
-
+document.getElementById('confirmDeleting').addEventListener('click',()=>{
+    remove=true;
+    deleteInput();
+})
+document.getElementById('cancelDeleting').addEventListener('click',toggleConfirm)
+/*********************/
 function handleAddition(e){
     e.preventDefault();
     let toDo=task.value;
@@ -21,27 +33,25 @@ function handleAddition(e){
         count++;
         setTaskCounter();
         tasks.push({id:count,toDo});
-        
-        
+        localStorage.setItem("alldata",JSON.stringify(tasks));
     }
-   else alert('PLEASE ENTER TASK TO ADD!');
     clearInput();
-    console.log(tasks)
     displayTasks()
 }
 function handleCompletion(){
-    todos.onclick = function(event) {  
+    toDos.onclick = function(event) {  
         let btn;
         btn = event.target.closest('.complete');
         if(btn){            
             btn.parentElement.firstElementChild.classList.toggle('complete');
         }
-
     }
-    
 }
 function setTaskCounter(){
     counter.innerHTML=`You have ${count} pending tasks.`
+}
+function toggleConfirm(){
+        document.getElementById('overlay').classList.toggle('display');
 }
 function displayTasks(){
     let result='';
@@ -51,15 +61,19 @@ function displayTasks(){
     <button onclick="handleCompletion()" class="complete"><i class="far fa-check-circle"></i></button> <button
             class="delete" onclick="deleteInput(${i})"><i class="fas fa-trash-alt"></i></button>
 </li>`);
-todos.innerHTML=result;
+toDos.innerHTML=result;
 }
 function deleteInput(id){
-    if(confirm('Are you sure delete this task?')){
+    toggleConfirm();
+    if(remove){
+    search.value=''
     tasks.splice(id,1);
+    localStorage.setItem("alldata",JSON.stringify(tasks));
     displayTasks();
     count--;
     setTaskCounter()
     }
+    remove=false
     }
 function clearInput(){
     task.value="";
@@ -76,5 +90,5 @@ function searchTasks(){
                     class="delete"  onclick="deleteInput(${i})"><i class="fas fa-trash-alt"></i></button>
         </li>`
     }});
-    todos.innerHTML=result;
+    toDos.innerHTML=result;
 }
